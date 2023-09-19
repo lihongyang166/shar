8635fa1edb965c2c7e5688094cd2f81976d5dcc3
JASON TEH <jasteh@gmail.com>
2023-09-19 14:40:01 +0100

    validate against different workflows containing a process with the same name

---
+++ integration/shar/process_test.go
@@ -0,0 +1,48 @@
+package intTest
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"gitlab.com/shar-workflow/shar/client"
+	"gitlab.com/shar-workflow/shar/client/taskutil"
+	support "gitlab.com/shar-workflow/shar/integration-support"
+	"os"
+	"testing"
+)
+
+func TestProcessPersistenceNonUniqueName(t *testing.T) {
+	tst := &support.Integration{}
+	//tst.WithTrace = true
+
+	tst.Setup(t, nil, nil)
+	defer tst.Teardown()
+
+	// Create a starting context
+	ctx := context.Background()
+
+	// Dial shar
+	cl := client.New(client.WithEphemeralStorage(), client.WithConcurrency(10))
+	err := cl.Dial(ctx, tst.NatsURL)
+	require.NoError(t, err)
+
+	// Register a service task
+	d := &testSimpleHandlerDef{t: t, finished: make(chan struct{})}
+	err = taskutil.RegisterTaskYamlFile(ctx, cl, "simple_test.yaml", d.integrationSimple)
+	require.NoError(t, err)
+
+	// Load BPMN workflow
+	b, err := os.ReadFile("../../testdata/simple-workflow.bpmn")
+	require.NoError(t, err)
+
+	_, err = cl.LoadBPMNWorkflowFromBytes(ctx, "SimpleWorkflowTest", b)
+	require.NoError(t, err)
+
+	//load another process with different workflow name but same process name
+	pb, err := os.ReadFile("../../testdata/simple-workflow-same-processname.bpmn")
+	require.NoError(t, err)
+
+	_, err = cl.LoadBPMNWorkflowFromBytes(ctx, "SimpleWorkflowSameProcessNameTest", pb)
+	require.Error(t, err)
+	require.ErrorContains(t, err, "[process: SimpleProcess, workflow: SimpleWorkflowTest]")
+
+}

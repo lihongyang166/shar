b9c72a6630dfe4fc49a7a75013a92292065023d0
Andrei Baker <andreib@crystalconstruct.com>
2024-03-19 18:09:04 +0000

    fix version

--- common/version/nats.go
+++ common/version/nats.go
@@ -1,7 +1,6 @@
 package version
 
-// Version is the application version overwritten during build.
-// During development this should be set to the minimum compatible version.
-//
-//goland:noinspection GoUnnecessarilyExportedIdentifiers
-var Version = "v1.1.670"
+import version2 "github.com/hashicorp/go-version"
+
+// NatsVersion is the mandatory minimum version of NATS that is supported by SHAR
+var NatsVersion, _ = version2.NewVersion("v2.10.12")

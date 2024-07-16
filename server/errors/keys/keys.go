57b05308146dd3af0fb0fdf8054137e603dac097
JASON TEH <jasteh@gmail.com>
2024-07-16 15:03:55 +0100

    use consistent abbreviations for certain terms in keys like workflow (wf), process instance (pi).

--- server/errors/keys/keys.go
+++ server/errors/keys/keys.go
@@ -11,9 +11,9 @@ const (
 	// ElementType is the key for the BPMN name for the element.
 	ElementType = "el_type"
 	// ProcessInstanceID is the key for the unique identifier for the executing workflow instance.
-	ProcessInstanceID = "process_instance_id"
+	ProcessInstanceID = "pi_id"
 	// WorkflowID is the key for the originating versioned workflow that started the instance.
-	WorkflowID = "workflow_id"
+	WorkflowID = "wf_id"
 	// ActivityID is the key for the name of the activity ID associated with an executing Job.
 	ActivityID = "job_activity_id"
 	// JobType is the key for the type of executing job.
@@ -21,11 +21,11 @@ const (
 	// JobID is the key for the executing job ID.
 	JobID = "job_id"
 	// ParentProcessInstanceID is the key for the parent process instance if this is a spawned process
-	ParentProcessInstanceID = "parent_process_instance_id"
+	ParentProcessInstanceID = "parent_pi_id"
 	// WorkflowName is the key for the originator workflow name.
-	WorkflowName = "workflow_name"
+	WorkflowName = "wf_name"
 	// ParentInstanceElementID is the key for the element in the parent instance that launched a sub workflow instance.
-	ParentInstanceElementID = "parent_element_id"
+	ParentInstanceElementID = "parent_el_id"
 	// Execute is a key that is used for the execution command of a Job or workflow activity.
 	Execute = "wf_ex"
 	// Condition is a key for a business rule to evaluate.
@@ -33,11 +33,11 @@ const (
 	// State is a key for the description of the current execution state of a workflow instance.
 	State = "el_state"
 	// TrackingID is a key for the unique ID for the executing state.
-	TrackingID = "tracking_id"
+	TrackingID = "tr_id"
 	// ParentTrackingID is a key for the parent of the current executing state.
-	ParentTrackingID = "parent_tracking_id"
+	ParentTrackingID = "parent_tr_id"
 	// ExecutionID is a key to correlate a process and any spawned sub process with each other
-	ExecutionID = "execution_id"
-	// ProcessId is a key for the unique name of a process
-	ProcessId = "process_id"
+	ExecutionID = "exec_id"
+	// ProcessId is a key for the unique id of a process
+	ProcessId = "p_id"
 )

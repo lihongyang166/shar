9291e08951e096d34a88f933b47852c37d1870e0
JASON TEH <jasteh@gmail.com>
2024-10-03 13:29:41 +0100

    don't close the err chan in the history method, the streaming framework will close it for you

--- internal/server/workflow/history.go
+++ internal/server/workflow/history.go
@@ -156,7 +156,7 @@ func (s *Operations) GetProcessHistory(ctx context.Context, processInstanceId st
 		}
 		wch <- entry
 	}
-	close(errs)
+	//close(errs)
 }
 
 // GetProcessHistoryItem retrieves a process history entry based on the given process instance ID, tracking ID, and history type.
